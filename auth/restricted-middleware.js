a83d21f98d4742c030272630a893523db3e161e2
vpagano10 <vpagano10@gmail.com>
2019-12-18 16:00:20 -0500

    done with mvp

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+   
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401)
+          .json({ message: 'Invalid Token' })
+        } else {
+          req.token = decodedToken;
+          next()
+        }
+    })
+  } else {
+    res.status(500)
+      .json({ message: 'Please try to login again' })
+  }
+};
